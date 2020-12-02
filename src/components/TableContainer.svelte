133cce7d7dacfff1212230e176dca9d85f4976d4
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-02 23:20:32 +0100

    Added table filter

--- src/components/TableContainer.svelte
+++ src/components/TableContainer.svelte
@@ -4,8 +4,24 @@
 
 	export let data;
 
-	$: states = data;
+	let stateName = '';
+	let sortBy = 'abbrev';
+	$: states = filterAndSort(data, stateName, sortBy);
+
+	function filterAndSort(states, stateName, sortBy) {
+		const filteredStates = states.filter(state => {
+			return stateName === '' || state.name.toLowerCase().startsWith(stateName.toLowerCase())
+		})
+
+		if(sortBy !== 'name') {
+			return filteredStates.sort((a,b) => {
+				return +b[sortBy] - +a[sortBy];
+			});
+		}
+
+		return filteredStates;
+	}
 </script>
 
-<TableFilter/>
+<TableFilter bind:stateName bind:sortBy/>
 <Table {states}/>
--- src/components/TableFilter.svelte
+++ src/components/TableFilter.svelte
@@ -1,1 +1,34 @@
-<h1>Table filter</h1>
+<script>
+	export let stateName = '';
+	export let sortBy = 'abbrev';
+</script>
+
+<style>
+	input, .select, select {
+		width: 100%;
+	}
+
+	.column {
+		padding-left: 0;
+	}
+</style>
+
+<div class="section">
+	<div class="container">
+		<div class="columns">
+			<div class="column is-6">
+				<input type="text" placeholder="Filter States" class="input" bind:value={stateName}>
+			</div>
+			<div class="column is-6">
+				<div class="select">
+					<select bind:value={sortBy}>
+						<option value="abbrev">By State Name</option>
+						<option value="cases">Cases</option>
+						<option value="deaths">Deaths</option>
+						<option value="tested">Tested</option>
+					</select>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
